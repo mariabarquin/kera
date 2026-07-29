7d32d625ddb2ea3b7a7ecd2160cf9aa75b6342cc
Maria <mariabarquin1516@alumnos.cei.es>
2026-07-29 11:38:35 +0200

    Fix: Export app for Vercel serverless functions

--- index.js
+++ index.js
@@ -54,3 +54,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
+
+//Instancia de Express para Vercel ejecutarla
+module.exports = app;
